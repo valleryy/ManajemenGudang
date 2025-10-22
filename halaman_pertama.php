fdcbfd63e5a7ef25a1f25fde68565b533feb7a2b
valleryy <valleryy62@gmail.com>
2025-10-22 11:25:47 +0700

    file1

---
+++ halaman_pertama.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>GudangSembako</title>
+</head>
+<body>
+	<div class="logo">
+		<img src="np.jpg">
+		<h3>NUSANTARA PANGAN</h3>
+		<button type="submit">Berikutnya</button>
+	</div>
+
+	
+</body>
+</html>
